13c293eae23312e1cf8e3a0946491107aa275aa8
Mohamed <88803877+mohamedlemroudi@users.noreply.github.com>
2022-03-10 07:50:38 +0100

    Add files via upload

---
+++ chain_methods.py
@@ -0,0 +1,25 @@
+# ----------------- Validar edad -----------------
+# edad = input("Introduce la edad: ")
+
+# while(edad.isdigit() == False):
+#     print("Por favor, introduce un valor numérico")
+
+#     edad = input("Introduce la edad: ")
+
+# if(int(edad)<18):
+#     print("No puede pasar")
+# else:
+#     print("puede passar")
+
+
+# ----------------- Programa @ -----------------
+email = input("Introduce un correo electronico: ")
+
+emailVarificado = True
+
+arroba = email.count('@')
+
+if (arroba != 0 or email.find('@') == 0 or email.rfind('@') == (len(email) - 1) or email.find('@') == -1):
+        print("El correo no es correcto")
+else:
+        print("El correo es correcto")
